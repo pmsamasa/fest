f29c86b8efef882af0f23ebf33bcdac404d79ece
mohdshuhaib <mohdshuhaib357@gmail.com>
2024-01-20 06:51:12 +0530

    mail ready and change email to phone

--- masa fest/js/script.js
+++ masa fest/js/script.js
@@ -7,17 +7,17 @@ window.addEventListener("load", () => {
         document.querySelector(".js-preloader").style.display = "none";
         //INITIALIZE AOS
         AOS.init({
-          duration: 1200,
-          easing:'ease-in-out-cubic',
-          once: true
+            duration: 1200,
+            easing: 'ease-in-out-cubic',
+            once: true
         });
-      });
     });
+});
 
 
 /* header menu */
 
-function headerMenu(){
+function headerMenu() {
     const toggler = document.querySelector(".js-header-toggler");
     const menu = document.querySelector(".js-header-menu");
     const items = menu.querySelectorAll("li");
@@ -30,7 +30,7 @@ function headerMenu(){
 
     items.forEach((item) => {
         item.querySelector("a").addEventListener("click", () => {
-            if(window.innerWidth <= 991){
+            if (window.innerWidth <= 991) {
                 menuToggle();
             }
         });
@@ -41,12 +41,12 @@ headerMenu();
 /* off stage / on stage */
 
 //show active color in onstage and offstage
-function scheduleTabes(){
+function scheduleTabes() {
     const tabes = document.querySelectorAll(".js-schedule-tabe");
 
     tabes.forEach((tabe) => {
         tabe.addEventListener("click", () => {
-            if(tabe.classList.contains("active")){
+            if (tabe.classList.contains("active")) {
                 return;
             }
             document.querySelector(".js-schedule-tabe.active").classList.remove("active");
@@ -63,13 +63,13 @@ document.getElementById('scheduleOffstage').style.display = 'block';
 function showstage(schedule) {
     // Hide all month containers and additional
     var stageContainers = document.querySelectorAll('.schedule-tabs');
-    stageContainers.forEach(function(container) {
+    stageContainers.forEach(function (container) {
         container.style.display = 'none';
     });
 
     var additional = document.querySelectorAll('.schedule-content');
-    additional.forEach(function(content) {
-    content.style.display = 'none';
+    additional.forEach(function (content) {
+        content.style.display = 'none';
     });
 
 
@@ -78,13 +78,13 @@ function showstage(schedule) {
     if (selectedstage) {
         selectedstage.style.display = 'block';
     }
-    
+
 
     // Show the additional content for the selected month
     var additionalContent = document.getElementById('schedule' + schedule.charAt(0).toUpperCase() + schedule.slice(1));
-            if (additionalContent) {
-                additionalContent.style.display = 'block';
-            }
+    if (additionalContent) {
+        additionalContent.style.display = 'block';
+    }
 }
 
 /*schedule timings */
@@ -92,145 +92,145 @@ function showstage(schedule) {
 let SHEET_ID = '1NTtWnFGtyC9K2qA6GnvgEk1wkIOpe4myKEJDRuNX1ZM';
 let SHEET_TITLE = 'time';
 let SHEET_RANGE = 'A2:C12';
-FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE );
+FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
 fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-    let data = JSON.parse(rep.substr(47).slice(0, -2));
-    let length = data.table.rows.length;//total number of values
+    .then(res => res.text())
+    .then(rep => {
+        let data = JSON.parse(rep.substr(47).slice(0, -2));
+        let length = data.table.rows.length;//total number of values
 
-    var table = document.getElementById('day-1');// table selector
+        var table = document.getElementById('day-1');// table selector
 
-    for (let i = 0; i < length; i++ ) {
-         
-        var newRow = table.insertRow();// insert rows
+        for (let i = 0; i < length; i++) {
 
-        var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+            var newRow = table.insertRow();// insert rows
 
-        cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
-        cell2.innerHTML = data.table.rows[i].c[1].v;
-        cell3.innerHTML = data.table.rows[i].c[2].v;
-    }
-    table.deleteRow(1)
+            var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
 
-})
+            cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
+            cell2.innerHTML = data.table.rows[i].c[1].v;
+            cell3.innerHTML = data.table.rows[i].c[2].v;
+        }
+        table.deleteRow(1)
+
+    })
 
 SHEET_RANGE = 'E2:G12';
-FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE );
+FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
 fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-    let data = JSON.parse(rep.substr(47).slice(0, -2));
-    let length = data.table.rows.length;//total number of values
+    .then(res => res.text())
+    .then(rep => {
+        let data = JSON.parse(rep.substr(47).slice(0, -2));
+        let length = data.table.rows.length;//total number of values
 
-    var table = document.getElementById('day-2');// table selector
+        var table = document.getElementById('day-2');// table selector
 
-    for (let i = 0; i < length; i++ ) {
-         
-        var newRow = table.insertRow();// insert rows
+        for (let i = 0; i < length; i++) {
 
-        var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+            var newRow = table.insertRow();// insert rows
 
-        cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
-        cell2.innerHTML = data.table.rows[i].c[1].v;
-        cell3.innerHTML = data.table.rows[i].c[2].v;
-    }
-    table.deleteRow(1)
+            var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
 
-})
+            cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
+            cell2.innerHTML = data.table.rows[i].c[1].v;
+            cell3.innerHTML = data.table.rows[i].c[2].v;
+        }
+        table.deleteRow(1)
+
+    })
 
 SHEET_RANGE = 'I2:K12';
-FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE );
+FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
 fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-    let data = JSON.parse(rep.substr(47).slice(0, -2));
-    let length = data.table.rows.length;//total number of values
+    .then(res => res.text())
+    .then(rep => {
+        let data = JSON.parse(rep.substr(47).slice(0, -2));
+        let length = data.table.rows.length;//total number of values
 
-    var table = document.getElementById('day-3');// table selector
+        var table = document.getElementById('day-3');// table selector
 
-    for (let i = 0; i < length; i++ ) {
-         
-        var newRow = table.insertRow();// insert rows
+        for (let i = 0; i < length; i++) {
 
-        var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+            var newRow = table.insertRow();// insert rows
 
-        cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
-        cell2.innerHTML = data.table.rows[i].c[1].v;
-        cell3.innerHTML = data.table.rows[i].c[2].v;
-    }
-    table.deleteRow(1)
+            var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
 
-})
+            cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
+            cell2.innerHTML = data.table.rows[i].c[1].v;
+            cell3.innerHTML = data.table.rows[i].c[2].v;
+        }
+        table.deleteRow(1)
+
+    })
 SHEET_RANGE = 'M2:O12';
-FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE );
+FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
 fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-    let data = JSON.parse(rep.substr(47).slice(0, -2));
-    let length = data.table.rows.length;//total number of values
+    .then(res => res.text())
+    .then(rep => {
+        let data = JSON.parse(rep.substr(47).slice(0, -2));
+        let length = data.table.rows.length;//total number of values
 
-    var table = document.getElementById('onday-1');// table selector
+        var table = document.getElementById('onday-1');// table selector
 
-    for (let i = 0; i < length; i++ ) {
-         
-        var newRow = table.insertRow();// insert rows
+        for (let i = 0; i < length; i++) {
 
-        var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+            var newRow = table.insertRow();// insert rows
 
-        cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
-        cell2.innerHTML = data.table.rows[i].c[1].v;
-        cell3.innerHTML = data.table.rows[i].c[2].v;
-    }
-    table.deleteRow(1)
+            var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
 
-})
+            cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
+            cell2.innerHTML = data.table.rows[i].c[1].v;
+            cell3.innerHTML = data.table.rows[i].c[2].v;
+        }
+        table.deleteRow(1)
+
+    })
 SHEET_RANGE = 'Q2:S12';
-FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE );
+FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
 fetch(FULL_URL)
-.then(res => res.text())
-.then(rep => {
-    let data = JSON.parse(rep.substr(47).slice(0, -2));
-    let length = data.table.rows.length;//total number of values
+    .then(res => res.text())
+    .then(rep => {
+        let data = JSON.parse(rep.substr(47).slice(0, -2));
+        let length = data.table.rows.length;//total number of values
 
-    var table = document.getElementById('onday-2');// table selector
+        var table = document.getElementById('onday-2');// table selector
 
-    for (let i = 0; i < length; i++ ) {
-         
-        var newRow = table.insertRow();// insert rows
+        for (let i = 0; i < length; i++) {
 
-        var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+            var newRow = table.insertRow();// insert rows
 
-        cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
-        cell2.innerHTML = data.table.rows[i].c[1].v;
-        cell3.innerHTML = data.table.rows[i].c[2].v;
-    }
-    table.deleteRow(1)
+            var cell1 = newRow.insertCell(0);//inserting colums/cells to above row
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
 
-})
+            cell1.innerHTML = data.table.rows[i].c[0].v;// setting value to the above cells
+            cell2.innerHTML = data.table.rows[i].c[1].v;
+            cell3.innerHTML = data.table.rows[i].c[2].v;
+        }
+        table.deleteRow(1)
+
+    })
 /* schedule tabs */
 
-function scheduleTabs(){
+function scheduleTabs() {
     const tabs = document.querySelectorAll(".js-schedule-tab");
 
     tabs.forEach((tab) => {
         tab.addEventListener("click", () => {
-            if(tab.classList.contains("active")){
+            if (tab.classList.contains("active")) {
                 return;
             }
             document.querySelector(".js-schedule-tab.active").classList.remove("active");
@@ -245,12 +245,12 @@ scheduleTabs();
 
 /* schedule tabs2 */
 
-function scheduleTabs2(){
+function scheduleTabs2() {
     const tabs = document.querySelectorAll(".js-schedule2-tab");
 
     tabs.forEach((tab) => {
         tab.addEventListener("click", () => {
-            if(tab.classList.contains("active")){
+            if (tab.classList.contains("active")) {
                 return;
             }
             document.querySelector(".js-schedule2-tab.active").classList.remove("active");
@@ -270,31 +270,31 @@ Splitting();
 
 const form = document.querySelector('form');
 const fullName = document.getElementById('name');
-const mail = document.getElementById('mail');
+const phone = document.getElementById('phone');
 const sub = document.getElementById('subject');
 const text = document.getElementById('textarea');
 
-function sendEmail(){
-    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${mail.value}<br> Subject: ${text.value}`;
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Phone: ${phone.value}<br> Subject: ${text.value}`;
 
     Email.send({
         SecureToken: "b7e1cefb-8dd1-477d-9397-d00d1079cb8f",
-        Username : "fest.masa2024@gmail.com",
-        Password : "5FF5580EC1CB7E3192ED5C4CEBF746E411CD",
-        To : 'fest.masa2024@gmail.com',
-        From : "fest.masa2024@gmail.com",
-        Subject : sub.value,
-        Body : bodyMessage
+        Username: "fest.masa2024@gmail.com",
+        Password: "5FF5580EC1CB7E3192ED5C4CEBF746E411CD",
+        To: 'fest.masa2024@gmail.com',
+        From: "fest.masa2024@gmail.com",
+        Subject: sub.value,
+        Body: bodyMessage
     }).then(
-      message => {
-        if (message == "OK") {
-            Swal.fire({
-                title: "Thank You!",
-                text: "Feedback send successfully!",
-                icon: "success"
-              });
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Thank You!",
+                    text: "Feedback send successfully!",
+                    icon: "success"
+                });
+            }
         }
-      }
     );
 
 }
@@ -307,14 +307,6 @@ function checkinputs() {
             item.classList.add('error');
             item.parentElement.classList.add('error');
         }
-
-        if (items[1].value != "") {
-            checkEmail();
-        }
-
-        items[1].addEventListener("keyup", () => {
-            checkEmail();
-        });
 
         item.addEventListener("keyup", () => {
             if (item.value != "") {
@@ -330,33 +322,11 @@ function checkinputs() {
 
 }
 
-function checkEmail(){
-    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
-
-    const errorTxTmail = document.querySelector(".error-text.email");
-
-    if (!mail.value.match(emailRegex)) {
-        mail.classList.add("error");
-        mail.parentElement.classList.add("error");
-
-        if (mail.value != "") {
-            errorTxTmail.innerText = "Enter a valid email address";
-        }
-        else {
-            errorTxTmail.innerText = "Email can't be blank";
-        }
-    }
-    else {
-        mail.classList.remove("error");
-        mail.parentElement.classList.remove("error");
-    }
-}
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkinputs();
 
-    if (!fullName.classList.contains("error") && !mail.classList.contains("error") && !sub.classList.contains("error") && !text.classList.contains("error")){
+    if (!fullName.classList.contains("error") && !phone.classList.contains("error") && !sub.classList.contains("error") && !text.classList.contains("error")) {
         sendEmail();
 
         form.reset();
